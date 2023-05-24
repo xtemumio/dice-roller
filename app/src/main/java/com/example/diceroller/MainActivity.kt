9e3e030cb811f207d31e834db9b86a75c82e1a4d
xtemumio <mpaolo025@gmail.com>
2023-05-24 20:07:29 +0200

    upload

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+
+        }
+
+        }
+    }
